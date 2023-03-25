868c5aada1c873d540d61c92ee9ae2c33bd25e85
Esmaelmohamed <asmaelmohamed502@gmail.com>
2023-03-24 18:41:46 -0700

    updated

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,11 +1,11 @@
-nclude "main.h"
-
+#include "main.h"
 /**
-   * main - check the code
-    *
-     * Return: Always 0.
-      */
-int main(void) {
-	  print_most_numbers();
-	    return (0);
+* main - check the code*
+* Return: Always 0.
+*/
+int main(void)
+
+{
+	print_most_numbers();
+	return (0);
 }
