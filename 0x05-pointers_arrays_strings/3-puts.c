1e5dd1bef366c395c4694073dd36c4a0f7683ea4
Esmaelmohamed <asmaelmohamed502@gmail.com>
2023-03-27 15:10:44 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,9 @@
-nclude "main.h"
-    /**
-     * _puts - prints a string, followed by a new line, to stdout
-     * @str: string to print
-     */
-    void
-    _puts(char *str) {
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str) {
   while (*str != '\0') {
     _putchar(*str++);
   }
